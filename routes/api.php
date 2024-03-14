ccb9cee4cb7b59b3802809afc79a2085c3fe370e
elo <124618520+eloa130906@users.noreply.github.com>
2024-03-14 10:05:58 -0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-<<<<<<< HEAD
 use App\Http\Controllers\StudentController; // Adicione a importação do StudentController
 
 /*
@@ -16,9 +15,6 @@ use App\Http\Controllers\StudentController; // Adicione a importação do Studen
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-=======
-use App\Http\Controllers\StudentController;
->>>>>>> c96ad9c40f0ab0b75aa731cd1db70467daf4b398
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -31,18 +27,9 @@ Route::get('/', function () {
     ]);
 });
 
-<<<<<<< HEAD
 Route::post('/rota/post', [HomeController::class, "index"]);
+
 
 Route::put("/atualizar/aluno/{id}", [StudentController::class, "change"]);
 
 Route::delete("/excluir/aluno/{id}", [StudentController::class, "delete"]);
-=======
-Route::post('/rota/post', [	HomeController::class,"index"]);
-
-Route::get('/students/{id}', [StudentController::class, 'show']);
-
-Route::get('/students', [StudentController::class, 'index']);
-
-Route::post('/students', [StudentController::class, 'store']);
->>>>>>> c96ad9c40f0ab0b75aa731cd1db70467daf4b398
